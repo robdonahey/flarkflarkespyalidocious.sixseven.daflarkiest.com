a5d811a659bf034a447853616e7bad99e6d145fd
Rob Donahey <rob.donahey@safelite.com>
2026-07-13 16:35:59 -0400

    Start or restart audio on click (#5)

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ const image = document.getElementById("bryant");
 const audio = document.getElementById("audio");
 
 image.addEventListener("click", () => {
-  if (audio.paused) {
-    audio.currentTime = 0;
-    audio.play();
-  } else {
-    audio.pause();
-    audio.currentTime = 0;
-  }
+  audio.currentTime = 0;
+  audio.play();
 });
